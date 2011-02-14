62d82cec3259f3605844b97743d61b82933afa61
rosanocangussu <rosanocangussu@gmail.com>
2011-02-13 23:15:00 -0200

    Iniciando

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nte::Application.config.secret_token = 'e6c1b943bdaebc8b211c8535f126751edde100602b27519c79a4299cb326edc0da0d4b69d7f5dad2084763218a1095cc796f9bec4a2c55b0f5f9027162a46826'
